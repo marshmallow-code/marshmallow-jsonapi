253113a560d982191e4f063744865b6a66b98065
Anders Steinlein <anders@e5r.no>
2017-06-26 13:55:32 +0200

    Pass context from top-level schema to children
    Any context passed into the top-level schema will now be passed into
    schemas defined on relationships as well. This allows inner schemas to
    access and use any context.

--- marshmallow_jsonapi/fields.py
+++ marshmallow_jsonapi/fields.py
@@ -101,18 +101,22 @@ class Relationship(BaseRelationship):
 
     @property
     def schema(self):
+        context = self.parent.context if self.parent else {}
+
         if isinstance(self.__schema, SchemaABC):
             return self.__schema
         if isinstance(self.__schema, type) and issubclass(self.__schema, SchemaABC):
-            self.__schema = self.__schema()
+            self.__schema = self.__schema(context=context)
             return self.__schema
         if isinstance(self.__schema, basestring):
             if self.__schema == _RECURSIVE_NESTED:
                 parent_class = self.parent.__class__
-                self.__schema = parent_class(include_data=self.parent.include_data)
+                self.__schema = parent_class(
+                    include_data=self.parent.include_data,
+                    context=context)
             else:
                 schema_class = class_registry.get_class(self.__schema)
-                self.__schema = schema_class()
+                self.__schema = schema_class(context=context)
             return self.__schema
         else:
             raise ValueError(('A Schema is required to serialize a nested '
