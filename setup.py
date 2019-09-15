2e08ffe8f2d9c5e9db61e4509956f969ee951a03
Steven Loria <sloria1@gmail.com>
2019-09-15 10:38:23 -0400

    Update setup.py and remove conditional installs

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ from setuptools import setup, find_packages
 INSTALL_REQUIRES = ("marshmallow>=2.8.0",)
 EXTRAS_REQUIRE = {
     "tests": ["pytest", "mock", "faker==2.0.1", "Flask==1.1.1"],
-    "lint": [
-        "flake8==3.7.8",
-        'flake8-bugbear==19.8.0; python_version >= "3.5"',
-        "pre-commit~=1.17",
-    ],
+    "lint": ["flake8==3.7.8", "flake8-bugbear==19.8.0", "pre-commit~=1.17"],
 }
 EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["tests"] + EXTRAS_REQUIRE["lint"] + ["tox"]
 
@@ -30,9 +26,6 @@ def find_version(fname):
     return version
 
 
-__version__ = find_version("marshmallow_jsonapi/__init__.py")
-
-
 def read(fname):
     with open(fname) as fp:
         content = fp.read()
@@ -41,7 +34,7 @@ def read(fname):
 
 setup(
     name="marshmallow-jsonapi",
-    version=__version__,
+    version=find_version("marshmallow_jsonapi/__init__.py"),
     description="JSON API 1.0 (https://jsonapi.org) formatting with marshmallow",
     long_description=read("README.rst"),
     author="Steven Loria",
@@ -52,6 +45,7 @@ setup(
     include_package_data=True,
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
+    python_requires=">=3.6",
     license="MIT",
     zip_safe=False,
     keywords=(
@@ -62,12 +56,9 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     test_suite="tests",
     project_urls={
